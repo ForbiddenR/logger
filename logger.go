ca6284e16eefd0aa0394f98ed247c5a6c61a5eb4
ForbiddenR <hostyouposition@outlook.com>
2023-08-21 21:49:25 -0400

    encapsulate the logger

--- logger.go
+++ logger.go
@@ -28,8 +28,29 @@ func InitLogger() *Logger {
 	return logger
 }
 
+func (l *Logger) Debug(template string, args ...any) {
+	l.Sugar().Debugf(template, args...)
+}
+
+func (l *Logger) Warn(template string, args ...any) {
+	l.Sugar().Warnf(template, args...)
+}
+
+func (l *Logger) Info(template string, args ...any) {
+	l.Sugar().Infof(template, args...)
+}
+
+func (l *Logger) Error(err error) {
+	l.Sugar().Error(err.Error())
+}
+
+func (l *Logger) Panic(template string, args ...any) {
+	l.Sugar().Panicf(template, args...)
+}
+
 func (l *Logger) cores() zapcore.Core {
 	cores := make([]zapcore.Core, 0)
+	
 	cores = append(cores, zapcore.NewCore(zapcore.NewConsoleEncoder(newConsoleEncoderConfig()), consuleWS, zap.DebugLevel))
 
 	return zapcore.NewTee(cores...)
