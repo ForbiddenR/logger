00265d66e48b0cb91bb040864f34fa3740cb575b
ForbiddenR <hostyouposition@outlook.com>
2024-12-20 13:54:08 +0800

    add a specific log method

--- interface.go
+++ interface.go
@@ -10,3 +10,8 @@ type Producer interface {
 	SendLog(ctx context.Context, level zapcore.Level, message string) error
 	Close(timeoutMs int64)
 }
+
+type Event interface {
+	Short() string
+	Long() string
+}
